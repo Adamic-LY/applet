58ae71bb3a68003b077083405fc91f5cdd9afb1e
adamicLY <adamic@sina.com>
2020-05-19 11:26:31 +0800

    5-18

--- components/tabBar/index.js
+++ components/tabBar/index.js
@@ -1,3 +1,13 @@
 Page({
     mixins: [require('../../utils/themeChanged')],
+    data:{
+        activeindex:""
+    },
+    changeactiveindex(e){
+      // console.log(e.currentTarget.id)
+       this.setData({
+         activeindex: e.currentTarget.id
+       })
+       console.log(this.data.activeindex)
+    }
 });
--- pages/buynow/buynow.js
+++ pages/buynow/buynow.js
@@ -5,100 +5,63 @@ Page({
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
-
-=======
     buynumber: 1,
     ischecked:false,
     ischecked1: false
->>>>>>> 5-15,立即购买页面
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-<<<<<<< HEAD
-  onLoad: function (options) {
-=======
   onLoad: function(options) {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-<<<<<<< HEAD
-  onReady: function () {
-=======
   onReady: function() {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-<<<<<<< HEAD
-  onShow: function () {
-=======
   onShow: function() {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-<<<<<<< HEAD
-  onHide: function () {
-=======
   onHide: function() {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-<<<<<<< HEAD
-  onUnload: function () {
-=======
   onUnload: function() {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-<<<<<<< HEAD
-  onPullDownRefresh: function () {
-=======
   onPullDownRefresh: function() {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-<<<<<<< HEAD
-  onReachBottom: function () {
-=======
   onReachBottom: function() {
->>>>>>> 5-15,立即购买页面
 
   },
 
   /**
    * 用户点击右上角分享
    */
-<<<<<<< HEAD
-  onShareAppMessage: function () {
-
-  }
-=======
   onShareAppMessage: function() {
 
   },
@@ -134,5 +97,4 @@ Page({
     })
   }
   
->>>>>>> 5-15,立即购买页面
 })
